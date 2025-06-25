e309e1231b4db5721f6fa54e4e22b1ed322420f9
skumar7-c <satyendrakumar25588@gmail.com>
2025-06-25 15:06:49 +0530

    family.js

--- backend/models/family.js
+++ backend/models/family.js
@@ -1,1 +1,30 @@
+const mongoose = require('mongoose');
+
+const familySchema = new mongoose.Schema({
+  profileImage: String,
+  familyHead: String,
+  gender: String,
+  dob: Date,
+  phone: String,
+  email: String,
+  locality: String,
+  occupation: String,
+  gotra: String,
+  nativePlace: String,
+  bloodGroup: String,
+  address: String,
+  members: [
+    {
+      name: String,
+      relation: String,
+      age: Number,
+      maritalStatus: String,
+      bloodGroup: String,
+      qualification: String,
+      occupation: String
+    }
+  ]
+});
+
+module.exports = mongoose.model('Family', familySchema);
  
